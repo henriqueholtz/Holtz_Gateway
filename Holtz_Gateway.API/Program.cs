20df7c34e6a31bcf1008f6dd86776cbb23d9ade3
henriqueholtz <henrique_holtz@hotmail.com>
2021-11-17 22:30:27 -0300

    Create ServiceApi (01 and 02) and configura Json files per environment into Program.cs

--- Holtz_Gateway.API/Program.cs
+++ Holtz_Gateway.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Holtz_Gateway.API
@@ -12,6 +13,14 @@ namespace Holtz_Gateway.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json")
+                    .AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
